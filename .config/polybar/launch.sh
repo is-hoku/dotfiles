6e3ea29b3c7246d414c7e214fe75d3be2827384a
is-hoku <this@is-hoku.info>
2021-12-03 18:47:04 +0900

    add settings for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c ~/.config/polybar/config sakura &
+# polybar -c ~/.config/polybar/config sakura &
 
-echo "Polybar launched..."
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload sakura &
+	done
+else
+	polybar --reload sakura &
+	echo "Polybar launched..."
+fi
