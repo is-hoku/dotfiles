16cb5faab42f94ab1c5fb06c3050f7e996b59cc2
is-hoku <me@hokuishi.be>
2024-12-22 22:40:02 +0900

    add SKK-JISYO and convertion of punctuation

--- .config/nvim/lua/plugins/skkeleton.lua
+++ .config/nvim/lua/plugins/skkeleton.lua
@@ -1,10 +1,31 @@
 return {
 	"vim-skk/skkeleton",
-	dependencies = {'vim-denops/denops.vim'},
+	dependencies = { 'vim-denops/denops.vim' },
 	config = function()
 		vim.fn['skkeleton#config']({
-			globalDictionaries = { { '/usr/share/skk/SKK-JISYO.L', 'euc-jp' } },
-			eggLikeNewline = true
+			globalDictionaries = {
+				{ '/usr/share/skk/SKK-JISYO.L',          'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.propernoun', 'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.jinmei',     'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.geo',        'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.fullname',   'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.station',    'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.law',        'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.assoc',      'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.JIS2',       'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.JIS3_4',     'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.JIS2004',    'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.mazegaki',   'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.itaiji',     'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.lisp',       'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.wrong',      'euc-jp' },
+				{ '/usr/share/skk/SKK-JISYO.requested',  'euc-jp' },
+			},
+			eggLikeNewline = true,
+		})
+		vim.fn['skkeleton#register_kanatable']('rom', {
+			[','] = { '，' },
+			['.'] = { '．' },
 		})
 	end
 }
